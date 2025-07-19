07a213c43a88c6bef053bec763fc86a104d4464b
Saahiil Shaikh <sahil@Saahiils-MacBook-Pro.local>
2025-07-19 15:18:21 +0530

    AI-111491

--- src/components/ProductSummary/index.tsx
+++ src/components/ProductSummary/index.tsx
@@ -3,22 +3,20 @@ import ProductSummaryItem from "./Item";
 import "./styles.css";
 import { ProductSummaryProps } from "./types";
 
-const ProductSummary: React.FC<ProductSummaryProps> = ({ invoices }) => {
+const ProductSummary: React.FC<ProductSummaryProps> = ({ invoice }) => {
   return (
     <>
-      {invoices.map((product, index) => (
-        product.items.map((item, itemIndex) => (
+      {invoice.items.map((product, index) => (
           <ProductSummaryItem
-            key={`${index}-${itemIndex}`}
-            image={item.productImage}
-            subtotal={product.subTotal.toFixed(2)}
-            tax={product.salesTaxRate.toFixed(2)}
-            shipping={product.shippingCharge.toFixed(2)}
-            cashback={product.cashbackEarned.toFixed(2)}
-            total={product.total.toFixed(2)}
-            product={item}
+            key={`${index}-${index}`}
+            image={product.productImage}
+            subtotal={invoice.subTotal.toFixed(2)}
+            tax={invoice.salesTaxRate.toFixed(2)}
+            shipping={invoice.shippingCharge.toFixed(2)}
+            cashback={invoice.cashbackEarned.toFixed(2)}
+            total={invoice.total.toFixed(2)}
+            product={product}
           />
-        ))
       ))}
     </>
   );
