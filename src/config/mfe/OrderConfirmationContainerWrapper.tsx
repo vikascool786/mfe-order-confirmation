f1bb14f7c09e7b497874c695a5f8ba6097a9ecf3
vikas wankhede <vikasw@marketamerica.com>
2025-09-18 16:35:44 +0530

    develop merged

--- src/config/mfe/OrderConfirmationContainerWrapper.tsx
+++ src/config/mfe/OrderConfirmationContainerWrapper.tsx
@@ -373,7 +373,7 @@ const OrderConfirmationContainerWrapper = (appConfig: {
                   <Notification
                     icon="ChangeCircle"
                     title="Subscribe & Save 10%"
-                    link="nbts/account/autoship?ham=10"
+                    link="https://www.shop.com/nbts/account/autoship?ham=10"
                     message="Explore Subscribe & Save"
                   />
                   <Notification
