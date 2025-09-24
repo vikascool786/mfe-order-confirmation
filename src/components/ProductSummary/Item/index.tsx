75e80ffd7f94e2534c3aaace09038870ec752601
vikas wankhede <vikasw@marketamerica.com>
2025-09-25 00:34:07 +0530

    develop merged

--- src/components/ProductSummary/Item/index.tsx
+++ src/components/ProductSummary/Item/index.tsx
@@ -77,13 +77,13 @@ const ProductSummaryItem: React.FC<ExtendedProductSummaryItemProps> = ({
           {product.autoShipEnabled && product.autoShipFrequency > 0 && (
             <div className="item-autoship item-autoship-frequency-bold">
               <AutoshipIcon />
-{product.hasAutoShipDiscount === "0"
-  ? "Subscription"
-  : `Subscribe and Save${
-      shopperPortalData && shopperPortalData.autoShipDiscount > 0
-        ? ` ${shopperPortalData.autoShipDiscount}%`
-        : ""
-    }`}
+              {product.hasAutoShipDiscount === "0"
+                ? "Subscription"
+                : `Subscribe and Save${
+                    shopperPortalData && shopperPortalData.autoShipDiscount > 0
+                      ? ` ${shopperPortalData.autoShipDiscount}%`
+                      : ""
+              }`}
             </div>
           )}
           {product.autoShipFrequency > 0 && (
